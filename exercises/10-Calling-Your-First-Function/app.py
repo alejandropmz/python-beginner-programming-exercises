7b0e0c15c07b394383ab0901e5c331911d2fc69a
alejandropmz <alejandropacheco2497@gmail.com>
2022-12-15 04:48:24 +0000

    last commit

--- exercises/10-Calling-Your-First-Function/app.py
+++ exercises/10-Calling-Your-First-Function/app.py
@@ -4,3 +4,8 @@ def is_odd(my_number):
 
 def my_main_code():
     # your code here
+    result = is_odd(45345)
+    print(result)
+
+
+print(my_main_code())
--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,1 +1,17 @@
 # Your code here!!
+
+def sing():
+    song=""
+    for i in range(1,13):
+        if i == 5:
+            song+= "whisper words of wisdom, "
+        elif i == 11:
+            song+= "there will be an answer, "
+        elif i == 12:
+            song+= "let it be"
+        else:
+            song+= "let it be, "
+        
+    return song
+
+print(sing())
